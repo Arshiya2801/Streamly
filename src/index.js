af39c13111086cc7d49245677fd238d86d7ba8fd
Arshiya Singh <singharshiya28@gmail.com>
2025-07-20 01:24:08 +0530

    express setup

--- src/index.js
+++ src/index.js
@@ -8,8 +8,15 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT ||8000,()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
 
-
+.catch((error)=>{
+    console.log('failed',error)
+})
 
 
 
