e7f786b6867879162689f9c6752b54aa2a55f06a
Arshiya Singh <singharshiya28@gmail.com>
2025-08-25 17:07:11 +0530

    Initialize comment controller

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model";
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getVideoComments = async (req, res) => {
+
+}
+const addComment = async (req, res) => {
+
+}
+
+const updateComment = asyncHandler(async (req, res) => {
+    
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+     deleteComment
+    }
