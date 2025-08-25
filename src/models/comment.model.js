8d2446fd928876168cad1326970df9dfa5334591
Arshiya Singh <singharshiya28@gmail.com>
2025-08-25 16:23:48 +0530

    comment models generated

---
+++ src/models/comment.model.js
@@ -0,0 +1,21 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const comment= new Schema({
+        content: {
+            type: String,
+            required: true
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Comment= mongoose.model('Comment',comment)
